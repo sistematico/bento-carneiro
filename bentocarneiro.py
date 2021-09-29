e9b07e061fe8f1571671397e51d1ff8a3895b05e
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-09-29 14:22:52 -0400

    Update automático

---
+++ bentocarneiro.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from config import TOKEN
+
+def hello(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+updater = Updater(TOKEN)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
