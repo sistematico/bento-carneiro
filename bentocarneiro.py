5407f074c84595a3c0d798fd6a6a6f04e281dee3
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-09-29 15:38:08 -0400

    Update automático

--- bentocarneiro.py
+++ bentocarneiro.py
@@ -1,13 +1,44 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+from functools import wraps
 from config import TOKEN
 
+def send_action(action):
+    """Sends `action` while processing func command."""
+
+    def decorator(func):
+        @wraps(func)
+        def command_func(update, context, *args, **kwargs):
+            context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=action)
+            return func(update, context,  *args, **kwargs)
+        return command_func
+    
+    return decorator
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+@send_action(ChatAction.TYPING)
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+@send_action(ChatAction.TYPING)
+def delete(update: Update, context: CallbackContext) -> None:
+    #update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    #update.delete_message(chat_id=message.chat_id, message_id=message.message_id, *args, **kwargs)
+    update.delete_message(chat_id=message.chat_id, message_id=message.message_id)
+
 updater = Updater(TOKEN)
 
+updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, delete))
 
 updater.start_polling()
 updater.idle()
